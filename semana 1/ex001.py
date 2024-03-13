741545c37a4bf72628cfff04698aa8423cce26b5
Patricia Naomi <132932532+patinaomi@users.noreply.github.com>
2024-03-13 10:46:03 -0300

    Update and rename ex2..html to ex001.py

---
+++ semana 1/ex001.py
@@ -0,0 +1,23 @@
+# Entrada de dados
+
+texto = input('Digite algo: ')
+print(texto)
+
+print(y)
+print(type(y))
+
+b = float(y)
+print(b)
+print(type(b))
+
+# Para verificar o tipo da variável
+
+print(type(y))
+print(type(x))
+
+# Separador
+
+x = 10
+y = 22.2
+
+print(x, y, sep = ' ---- ')
