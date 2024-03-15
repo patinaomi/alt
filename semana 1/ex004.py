b846874acf642188551d2a56d0740d9a44f11f84
Patricia Naomi <132932532+patinaomi@users.noreply.github.com>
2024-03-15 10:26:13 -0300

    Update ex004.py

--- semana 1/ex004.py
+++ semana 1/ex004.py
@@ -1,3 +1,9 @@
-{
-    "liveServer.settings.port": 5501
-}
+while True:
+    st = input('Digite sim ou não: ')
+
+    if st.lower() == 'sim':
+        print('Você digitou sim')
+    elif st.lower() == 'não':
+        print('Você digitou não')
+    else:
+        print(f'Você digitou {st}')
